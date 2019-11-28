3e7f73a8628d4a050fa5626a516bb0cba5dc3548
Patrick Faion <pfa@pupil-labs.com>
2019-11-28 15:10:53 +0100

    Updated analysis

--- compare_detectors.py
+++ compare_detectors.py
@@ -1,5 +1,6 @@
-from pupil_detectors import Detector2D
+# from pupil_detectors import Detector2D
 from pure_detector import PuReDetector
+# from pure_original import PuReOriginal
 
 import LPW
 
@@ -9,8 +10,9 @@ import numpy as np
 import time
 
 
-pupil_d = Detector2D()
+# pupil_d = Detector2D()
 pure_d = PuReDetector()
+# pure_d = PuReOriginal()
 
 data = []
 
@@ -21,11 +23,10 @@ for subject, video_id, n, target, frame in LPW.video_iterator():
     frame = cv2.resize(frame, (320, 240))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # t1 = time.perf_counter()
+    t1 = time.perf_counter()
     # result_2d = pupil_d.detect(gray)
-    t2 = time.perf_counter()
     result_pure = pure_d.detect(gray)
-    t3 = time.perf_counter()
+    t2 = time.perf_counter()
 
 
 
@@ -48,24 +49,26 @@ for subject, video_id, n, target, frame in LPW.video_iterator():
         "frame": n,
         "target_x": target[0] / 2,
         "target_y": target[1] / 2,
-        # "2d.confidence": result_2d["confidence"],
-        # "2d.angle": result_2d["ellipse"]["angle"],
-        # "2d.first_ax": result_2d["ellipse"]["axes"][0],
-        # "2d.second_ax": result_2d["ellipse"]["axes"][1],
-        # "2d.center_x": result_2d["ellipse"]["center"][0],
-        # "2d.center_y": result_2d["ellipse"]["center"][1],
-        # "2d.time": t2 - t1,
+
+        # "confidence": result_2d["confidence"],
+        # "angle": result_2d["ellipse"]["angle"],
+        # "first_ax": result_2d["ellipse"]["axes"][0],
+        # "second_ax": result_2d["ellipse"]["axes"][1],
+        # "center_x": result_2d["ellipse"]["center"][0],
+        # "center_y": result_2d["ellipse"]["center"][1],
+
         "confidence": result_pure["confidence"],
         "angle": result_pure["angle"],
         "first_ax": result_pure["first_ax"],
         "second_ax": result_pure["second_ax"],
         "center_x": result_pure["center_x"],
         "center_y": result_pure["center_y"],
-        "time": t3 - t2,
-        "method": "pure.dirty_canny"
+
+        "time": t2 - t1,
+        "method": "pure.adjusted1",
     })
 
 df = pd.DataFrame(data)
 
-df.to_pickle("data.pure.dirty_canny.pkl")
+df.to_pickle("data.pure.adjusted1.pkl")
 
