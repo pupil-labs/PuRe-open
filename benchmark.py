e99e73e3a8b9be63a6e8faa1d26525171063d6eb
Patrick Faion <p.faion@web.de>
2020-01-20 11:01:28 +0100

    More elaborate benchmarking

--- benchmark.py
+++ benchmark.py
@@ -8,23 +8,34 @@ import cv2
 import pandas as pd
 import numpy as np
 import time
+import platform
 
 import LPW
-# LPW.LPW_path = "/media/pfa/P L/pfa/datasets/LPW"
 
 detectors = {
     "2d": Detector2D(),
     "pure_pfa": PuReDetector(),
     # "pure_orig": PuReOriginal(),
 }
+smalls = [
+    True,
+    # False,
+]
 
+computer = platform.node()
 
 for name, detector in detectors.items():
-    for small in [True, False]:
+    for small in smalls:
 
         data = []
 
-        method = f"{'small' if small else 'orig'}.{name}"
+        method = ".".join([
+            "benchmark",
+            "lpw",
+            "small" if small else "orig",
+            computer,
+            name,
+        ])
 
         for subject, video_id, n, target, frame in LPW.video_iterator():
             if n % 100 == 0:
@@ -44,21 +55,23 @@ for name, detector in detectors.items():
                 "frame": n,
                 "time": t2 - t1,
                 "method": method,
-                "target": target,
+                "target": np.array(target),
                 "confidence": result["confidence"],
             }
 
+            factor = 2.0 if small else 1.0
+
             if name != "pure_orig":
-                entry["center"] = np.array(result["ellipse"]["center"])
-                entry["axes"] = np.array(result["ellipse"]["axes"])
+                entry["center"] = np.array(result["ellipse"]["center"]) * factor
+                entry["axes"] = np.array(result["ellipse"]["axes"]) * factor
                 entry["angle"] = result["ellipse"]["angle"]
             else:
-                entry["center"] = np.array((result["center_x"], result["center_y"]))
-                entry["axes"] = np.array((result["first_ax"], result["second_ax"]))
+                entry["center"] = np.array((result["center_x"], result["center_y"])) * factor
+                entry["axes"] = np.array((result["first_ax"], result["second_ax"])) * factor
                 entry["angle"] = result["angle"]
 
             data.append(entry)
 
         df = pd.DataFrame(data)
-        df.to_pickle(f"benchmark.lpw.{method}.pkl")
+        df.to_pickle(f"{method}.pkl")
 
