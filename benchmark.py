40420cca0caf6e1f6f4095b30995e0dc1bebd7a7
Patrick Faion <p.faion@web.de>
2020-01-21 18:08:13 +0100

    Add full benchmarking script across sizes and data

--- benchmark.py
+++ benchmark.py
@@ -1,8 +1,7 @@
 from pupil_detectors import Detector2D
 from pure_detector import PuReDetector
-# from pure_original import PuReOriginal
+from pure_original import PuReOriginal
 
-import eye_seg
 
 import cv2
 import pandas as pd
@@ -11,67 +10,103 @@ import time
 import platform
 
 import LPW
+import eye_seg
+
+def LPW_iterator():
+    for subject, video_id, n, target, frame in LPW.video_iterator():
+        yield frame, target, f"{subject}.{video_id}.{n}"
+
+def eye_seg_iterator():
+    for part, n, raw_frame, center, axes, angle in eye_seg.video_iterator():
+        yield raw_frame, center, f"{part}.{n}"
+
+datasets = {
+    "lpw": LPW_iterator,
+    "eyeseg500k": eye_seg_iterator,
+}
 
 detectors = {
     "2d": Detector2D(),
     "pure_pfa": PuReDetector(),
-    # "pure_orig": PuReOriginal(),
+    "pure_orig": PuReOriginal(),
 }
-smalls = [
-    True,
-    # False,
+
+sizes = [
+    "original",
+    "192x192",
+    "320x240",
+]
+
+skip_patterns = [
+    "lpw",
 ]
 
 computer = platform.node()
 
-for name, detector in detectors.items():
-    for small in smalls:
+for dataset, iterator in datasets.items():
+    for name, detector in detectors.items():
+        for size in sizes:
+            method = ".".join([
+                "benchmark",
+                dataset,
+                size,
+                computer,
+                name,
+            ])
 
-        data = []
+            if any(pattern in method for pattern in skip_patterns):
+                print(f"Skipping '{method}'")
+                continue
 
-        method = ".".join([
-            "benchmark",
-            "lpw",
-            "small" if small else "orig",
-            computer,
-            name,
-        ])
+            data = []
 
-        for subject, video_id, n, target, frame in LPW.video_iterator():
-            if n % 100 == 0:
-                print(name, subject, video_id, n)
+            for i, (frame, target, data_key) in enumerate(iterator()):
+                if i % 100 == 0:
+                    print(method, i, data_key)
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            if small:
-                gray = cv2.resize(gray, (320, 240), interpolation=cv2.INTER_AREA)
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            t1 = time.perf_counter()
-            result = detector.detect(gray)
-            t2 = time.perf_counter()
+                area = gray.shape[0] * gray.shape[1]
+                if size == "original":
+                    factor = 1.0
+                elif size == "192x192":
+                    target_area = 192 * 192
+                    factor = np.sqrt(target_area / area)
+                elif size == "320x240":
+                    target_area = 320 * 240
+                    factor = np.sqrt(target_area / area)
+                    
+                if factor > 1.0:
+                    gray = cv2.resize(gray, dsize=None, fx=factor, fy=factor, interpolation=cv2.INTER_CUBIC)
+                elif factor < 1.0:
+                    gray = cv2.resize(gray, dsize=None, fx=factor, fy=factor, interpolation=cv2.INTER_AREA)
 
-            entry = {
-                "subject": subject,
-                "video_id": video_id,
-                "frame": n,
-                "time": t2 - t1,
-                "method": method,
-                "target": np.array(target),
-                "confidence": result["confidence"],
-            }
+                rescaling_factor = 1.0 / factor
 
-            factor = 2.0 if small else 1.0
 
-            if name != "pure_orig":
-                entry["center"] = np.array(result["ellipse"]["center"]) * factor
-                entry["axes"] = np.array(result["ellipse"]["axes"]) * factor
-                entry["angle"] = result["ellipse"]["angle"]
-            else:
-                entry["center"] = np.array((result["center_x"], result["center_y"])) * factor
-                entry["axes"] = np.array((result["first_ax"], result["second_ax"])) * factor
-                entry["angle"] = result["angle"]
+                t1 = time.perf_counter()
+                result = detector.detect(gray)
+                t2 = time.perf_counter()
 
-            data.append(entry)
+                entry = {
+                    "data_key": data_key,
+                    "time": t2 - t1,
+                    "method": method,
+                    "target": np.array(target),
+                    "confidence": result["confidence"],
+                }
 
-        df = pd.DataFrame(data)
-        df.to_pickle(f"{method}.pkl")
+                if name != "pure_orig":
+                    entry["center"] = np.array(result["ellipse"]["center"]) * rescaling_factor
+                    entry["axes"] = np.array(result["ellipse"]["axes"]) * rescaling_factor
+                    entry["angle"] = result["ellipse"]["angle"]
+                else:
+                    entry["center"] = np.array((result["center_x"], result["center_y"])) * rescaling_factor
+                    entry["axes"] = np.array((result["first_ax"], result["second_ax"])) * rescaling_factor
+                    entry["angle"] = result["angle"]
+
+                data.append(entry)
+
+            df = pd.DataFrame(data)
+            df.to_pickle(f"{method}.pkl")
 
