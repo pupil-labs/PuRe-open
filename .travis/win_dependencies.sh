bac7210b9ee290b444564f379f3d6ebdbe9ba734
Patrick Faion <pfa@pupil-labs.com>
2020-02-10 11:28:12 +0100

    Silence wget download

--- .travis/win_dependencies.sh
+++ .travis/win_dependencies.sh
@@ -9,7 +9,7 @@ cd dependencies
 # faster than compiling yourself. Note: The .exe is a 7-zip self extracting archive,
 # which explains the non-exe-standard cli argument -y. See here:
 # https://sevenzip.osdn.jp/chm/cmdline/switches/sfx.htm
-wget https://sourceforge.net/projects/opencvlibrary/files/4.2.0/opencv-4.2.0-vc14_vc15.exe
+wget -q https://sourceforge.net/projects/opencvlibrary/files/4.2.0/opencv-4.2.0-vc14_vc15.exe
 ./opencv-4.2.0-vc14_vc15.exe -y
 mv opencv opencvfull
 mv opencvfull/build opencv
