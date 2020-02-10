4816a29d41d674c3f6a184cc54e3e06e6b8ee4c5
Patrick Faion <pfa@pupil-labs.com>
2020-02-10 13:04:54 +0100

    Install and cache python

--- .travis/win_dependencies.sh
+++ .travis/win_dependencies.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 set -e
 
-if [[ -d dependencies/opencv ]]
-then
-    echo "Found cached OpenCV."
-    exit 0
-fi
-echo "Rebuilding OpenCV cache."
+
 
 mkdir -p dependencies
 cd dependencies
 
 # Opencv
-# Downloading the precompiled installer for windows and executing via cmd is actually
-# faster than compiling yourself. Note: The .exe is a 7-zip self extracting archive,
-# which explains the non-exe-standard cli argument -y. See here:
-# https://sevenzip.osdn.jp/chm/cmdline/switches/sfx.htm
-wget -q https://sourceforge.net/projects/opencvlibrary/files/4.2.0/opencv-4.2.0-vc14_vc15.exe
-./opencv-4.2.0-vc14_vc15.exe -y
-mv opencv opencvfull
-mv opencvfull/build opencv
-rm -rf opencvfull
-rm -rf opencv-4.2.0-vc14_vc15.exe
+if [[ -d opencv ]]
+then
+    echo "Found cached OpenCV."
+else
+    echo "Rebuilding OpenCV cache."
+    # Downloading the precompiled installer for windows and executing via cmd is actually
+    # faster than compiling yourself. Note: The .exe is a 7-zip self extracting archive,
+    # which explains the non-exe-standard cli argument -y. See here:
+    # https://sevenzip.osdn.jp/chm/cmdline/switches/sfx.htm
+    wget -q https://sourceforge.net/projects/opencvlibrary/files/4.2.0/opencv-4.2.0-vc14_vc15.exe
+    ./opencv-4.2.0-vc14_vc15.exe -y
+    mv opencv opencvfull
+    mv opencvfull/build opencv
+    rm -rf opencvfull
+    rm -rf opencv-4.2.0-vc14_vc15.exe
+fi
+
+# Python
+if [[ -d /c/Python${PY_MM} ]]
+then
+    echo "Found cached Python."
+    python --version
+else
+    echo "Installing Python ${PYTHON_VERSION} with choco."
+    choco install python --version $PYTHON_VERSION
+    python -m pip install -U pip
+fi
