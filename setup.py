69861f9a4f4ca7ab14abf581ab672c4bf541af71
Patrick Faion <pfa@pupil-labs.com>
2020-02-10 13:56:44 +0100

    Use MSVC on window builds explicitly

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from skbuild import setup
+import platform
 import subprocess
 
+from skbuild import setup
 
 package_dir = "src"
 package = "pure_detector"
@@ -20,9 +21,17 @@ with open(f"{package_dir}/{package}/_meta.py", "w") as f:
     f.writelines([f'git_hash = "{git_hash}"', ""])
 
 
+cmake_args = []
+if platform.system() == "Windows":
+    # The Ninja cmake generator will use mingw (gcc) on windows travis instances, but we
+    # need to use msvc for compatibility.
+    cmake_args.append("-GVisual Studio 15 2017 Win64")
+
+
 setup(
     author="Pupil Labs",
     author_email="info@pupil-labs.com",
+    cmake_args=cmake_args,
     install_requires=install_requires,
     license="GNU",
     name="pure_detector",
