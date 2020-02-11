f24d7502fc46096881c1e6f7ffbf54710b29827a
Patrick Faion <pfa@pupil-labs.com>
2020-02-11 18:50:54 +0100

    set ld_library_path for auditwheel

--- .travis/build_wheels_manylinux.sh
+++ .travis/build_wheels_manylinux.sh
@@ -11,13 +11,15 @@ pip install -U pip
 export OpenCV_DIR=/io/dependencies/opencv
 pip wheel /io -w /io/raw_wheels --no-deps
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/io/dependencies/opencv/lib64
 for whl in /io/raw_wheels/*.whl; do
     auditwheel repair "$whl" --plat manylinux2014_x86_64 -w /io/dist/
 done
+
+find /io/dist
 
 for whl in /io/dist/*.whl; do
     pip install "$whl"
 done
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/io/dependencies/opencv/lib64
 python -c "from pure_detector import PuReDetector"
