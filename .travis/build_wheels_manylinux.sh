43583338860d16db67d41d853c38f4b015df82ad
Patrick Faion <pfa@pupil-labs.com>
2020-02-11 18:46:09 +0100

    Move env variables to docker script

--- .travis/build_wheels_manylinux.sh
+++ .travis/build_wheels_manylinux.sh
@@ -7,6 +7,8 @@ export PATH=$python_bin_dir:$PATH
 
 pip install -U pip
 
+
+export OpenCV_DIR=/io/dependencies/opencv
 pip wheel /io -w /io/raw_wheels --no-deps
 
 for whl in /io/raw_wheels/*.whl; do
@@ -17,4 +19,5 @@ for whl in /io/dist/*.whl; do
     pip install "$whl"
 done
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/io/dependencies/opencv/lib64
 python -c "from pure_detector import PuReDetector"
