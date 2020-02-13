90b09f266cab7c599122e70e8036c75a33c2ef29
Patrick Faion <pfa@pupil-labs.com>
2020-02-13 12:36:13 +0100

    Getting rid of mac rpath

--- setup.py
+++ setup.py
@@ -38,17 +38,17 @@ elif platform.system() == "Darwin":
     # not support -std=c++17, but we are using LLVM 4.0 on travis, so we need to use
     # c++1z. See https://clang.llvm.org/cxx_status.html
     cmake_args.append('-DCMAKE_CXX_FLAGS="-std=c++1z"')
-    cmake_args.append("-DCMAKE_MACOSX_RPATH=0")
+    cmake_args.append('-DCMAKE_INSTALL_RPATH="@loader_path"')
 
 
 external_package_data = []
-# opencv_lib_path = os.environ.get("PURE_WHEEL_OPENCV_LIB_PATH", None)
-# if opencv_lib_path:
-#     lib_dir = Path(opencv_lib_path)
-#     if lib_dir.exists():
-#         for entry in lib_dir.iterdir():
-#             if entry.is_file() and entry.suffix in (".dll", ".so", ".dylib"):
-#                 external_package_data.append(entry)
+opencv_lib_path = os.environ.get("PURE_WHEEL_OPENCV_LIB_PATH", None)
+if opencv_lib_path:
+    lib_dir = Path(opencv_lib_path)
+    if lib_dir.exists():
+        for entry in lib_dir.iterdir():
+            if entry.is_file() and entry.suffix in (".dll", ".so", ".dylib"):
+                external_package_data.append(entry)
 
 
 @contextmanager
